53c60bba2fad65d6c0aa5f9d96b61b7567df619c
Honesh Rai Pareek <honeshraipareek@gmail.com>
2020-11-09 23:05:36 +0530

    Commit 3

--- inverse1.R
+++ inverse1.R
@@ -10,3 +10,15 @@ makeCacheMatrix <- function(x = matrix()) {
     getinverse <- function() inv                     
     list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)   
 }
+cacheSolve <- function(x, ...) {
+    
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
+}
